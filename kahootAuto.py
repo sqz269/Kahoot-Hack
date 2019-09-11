b7c7d4b01d8c820b8ba7edbe4e9bdbb45896074d
sqz269 <yin2690855381@gmail.com>
2019-09-11 16:32:13 -0500

    Added Time for js to do it's stuff, Added more exception handling so exception won't case it to exit

--- kahootAuto.py
+++ kahootAuto.py
@@ -10,13 +10,15 @@ init_logging()
 class GetAnswers():
 
 
-	def __init__(self, quizID):
+	def __init__(self, quizID, pageLoadTime):
 		self.logger = getLogger("bot")
 
 		self.quizID = quizID
 		self.logger.info("Got Quiz id: {}".format(quizID))
 
 		self.initWebDriver()
+
+		self.pageLoadTime = pageLoadTime
 
 		self.CVT_INT_SHAPE = {
 			3: "TRIANGLE",
@@ -32,8 +34,8 @@ class GetAnswers():
 			self.driver = webdriver.Chrome("chromedriver_76.exe") # TODO: Allow user to select chromedriver version
 		except common.exceptions.WebDriverException as err:
 			self.logger.error("ERROR: {}".format(err))
-			if ("binary" in str(err)):
-				self.logger.error("Failed to locate Chrome binary.")
+			self.logger.critical("FAILED TO START AUTOMATED BROWSER SESSION; UNABLE TO CONTINUE\nPress Enter to exit")
+			input(); raise SystemExit(1);
 
 		self.logger.info("Automated Browser Session Initalized")
 
@@ -61,7 +63,8 @@ class GetAnswers():
 		self.logger.info("Constructed Quiz Answer URL: {}".format(quizAnswersURL))
 		self.driver.get(quizAnswersURL)
 		self.logger.info("Clicking on Show Answers Button")
-		time.sleep(1)  # Pause and make javascript do the work
+		self.logger.info("Webpage Load complete; Wait constant amt({}s) for Js to load".format(self.pageLoadTime))
+		time.sleep(self.pageLoadTime)  # Pause and make javascript do the work
 		showAnswersBtn = self.driver.find_element_by_class_name("question-list__group-toggle")
 		showAnswersBtn.click()
 
@@ -69,12 +72,14 @@ class GetAnswers():
 class AutoPlay():
 
 
-	def __init__(self, gamePin, username, answerDelay, answerKey):
+	def __init__(self, gamePin, username, answerDelay, answerKey, pageLoadTime):
 		self.gamePin = gamePin
 		self.username = username
 		self.answerDelay = answerDelay
 		self.answerDict = answerKey
+		self.pageLoadTime = pageLoadTime
 		self.logger = getLogger("bot")
+		self.logger.warning("Answer Delay is set to {}s".format(answerDelay))
 		self.initWebDriver()
 
 		self.CVT_SHAPE_CLASSNAME = {
@@ -91,8 +96,8 @@ class AutoPlay():
 			self.driver = webdriver.Chrome("chromedriver_76.exe") # TODO: Allow user to select chromedriver version
 		except common.exceptions.WebDriverException as err:
 			self.logger.error("ERROR: {}".format(err))
-			if ("binary" in str(err)):
-				self.logger.error("Failed to locate Chrome binary.")
+			self.logger.critical("FAILED TO START AUTOMATED BROWSER SESSION; UNABLE TO CONTINUE\nPress Enter to exit")
+			input(); raise SystemExit(1);
 
 		self.logger.info("Automated Browser Session Initalized")
 
@@ -104,7 +109,8 @@ class AutoPlay():
 		self.driver.find_element_by_id("game-input").send_keys(self.gamePin)
 		self.driver.find_element_by_class_name("enter-button__EnterButton-sc-1o9b9va-0").click()
 		self.logger.info("Pressing Enter Button")
-		time.sleep(1)
+		self.logger.info("Webpage Load complete; Wait constant amt({}s) for Js to load".format(self.pageLoadTime))
+		time.sleep(self.pageLoadTime)
 		self.driver.find_element_by_id("nickname").send_keys(self.username)
 		self.logger.info("Entering Username")
 		self.driver.find_element_by_class_name("enter-button__EnterButton-sc-1o9b9va-0").click()
@@ -114,12 +120,15 @@ class AutoPlay():
 	def doGameLoop(self):
 		while True:
 			if (self.driver.current_url == "https://kahoot.it/v2/gameblock"):
-				questionNumberText = self.driver.find_element_by_class_name("question-top-bar__QuestionNumber-sc-1pwisow-3").text
-				self.logger.debug("Question number raw text form: {}".format(questionNumberText))
-				questionNumber = int(questionNumberText.split("of")[0])
-				self.logger.info("Current Question is: {} | Answer Shape: {}".format(questionNumber, self.answerDict.get(questionNumber)))
-				time.sleep(self.answerDelay)
-				self.driver.find_element_by_class_name(self.CVT_SHAPE_CLASSNAME.get(self.answerDict.get(questionNumber))).click()
+				try:
+					questionNumberText = self.driver.find_element_by_class_name("question-top-bar__QuestionNumber-sc-1pwisow-3").text
+					self.logger.debug("Question number raw text form: {}".format(questionNumberText))
+					questionNumber = int(questionNumberText.split("of")[0])
+					self.logger.info("Current Question is: {} | Answer Shape: {}".format(questionNumber, self.answerDict.get(questionNumber)))
+					time.sleep(self.answerDelay)
+					self.driver.find_element_by_class_name(self.CVT_SHAPE_CLASSNAME.get(self.answerDict.get(questionNumber))).click()
+				except Exception as e:
+					self.logger.exception("Failed to answer question number: {}; Supposed Answer: {}. Execution will continue".format(questionNumber, self.answerDict.get(questionNumber)))
 			else:
 				self.logger.debug("Not In Question stage. waiting")
 				time.sleep(0.5)
@@ -135,24 +144,25 @@ def startGameFromIni():
 	pin = mainCfg["GamePIN"]
 	username = mainCfg["Username"]
 	delay = float(mainCfg["AnswerDelay"])
-	AP = AutoPlay(pin, username, delay, getQuizAnswers(uuid))
+	pageLoadDelay = float(mainCfg["pgLoadDelay"])
+	AP = AutoPlay(pin, username, delay, getQuizAnswers(uuid, pageLoadDelay), pageLoadDelay)
 	AP.enterGame()
 	AP.doGameLoop()
 
 
-def getQuizAnswers(quizUUID):
-	GA = GetAnswers(quizUUID)
+def getQuizAnswers(quizUUID, loadDelay):
+	GA = GetAnswers(quizUUID, loadDelay)
 	GA.getQuizPage()
 	return GA.getQuizAnswers()
 
 
 def getUserInput():
 	quizUUID = input("Game UUID: ")
-
 	gamePin = input("Game Pin: ")
 	username = input("Username: ")
 	delay = float(input("Answer Delay: "))
-	AP = AutoPlay(gamePin, username, delay, getQuizAnswers(quizUUID))
+	pgLoadDelay = float(input("Click Element delay after page load: "))
+	AP = AutoPlay(gamePin, username, delay, getQuizAnswers(quizUUID, pgLoadDelay), pgLoadDelay)
 	AP.enterGame()
 	AP.doGameLoop()
 
